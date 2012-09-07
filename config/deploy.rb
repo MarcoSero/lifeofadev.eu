85c4f4f50eeeff264b8e5090398ae708a91a5f68
Marco Sero <marco@marcosero.com>
2012-09-07 18:57:41 +0200

    add cap files to deploy

---
+++ config/deploy.rb
@@ -0,0 +1,56 @@
+# be sure to change these
+set :user, 'ubuntu'
+set :domain, 'lifeofadev.marcosero.com'
+set :application, 'lifeofadev'
+
+# The location of the LOCAL repository relative to the current app
+set :repository,  "."
+# The way in which files will be transferred from repository to remote host
+# If you were using a hosted github repository this would be slightly different
+set :deploy_to, "/home/#{user}/public_html/#{application}"
+
+# distribute your applications across servers (the instructions below put them
+# all on the same server, defined above as 'domain', adjust as necessary)
+role :app, domain
+role :web, domain
+role :db, domain, :primary => true
+
+# you might need to set this if you aren't seeing password prompts
+#default_run_options[:pty] = true
+
+# As Capistrano executes in a non-interactive mode and therefore doesn't cause
+# any of your shell profile scripts to be run, the following might be needed
+# if (for example) you have locally installed gems or applications.  Note:
+# this needs to contain the full values for the variables set, not simply
+# the deltas.
+# default_environment['PATH']='<your paths>:/usr/local/bin:/usr/bin:/bin'
+# default_environment['GEM_PATH']='<your paths>:/usr/lib/ruby/gems/1.8'
+
+# miscellaneous options
+ssh_options[:user] = "ubuntu"
+ssh_options[:keys] = ["#{ENV['HOME']}.ssh/marcosero.pem"] # make sure you also have the publickey
+ssh_options[:forward_agent] = true
+
+set :deploy_via, :remote_cache
+set :scm, 'git'
+set :branch, 'master'
+set :scm_verbose, true
+set :use_sudo, false
+
+namespace :deploy do
+  desc "cause Passenger to initiate a restart"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt" 
+  end
+
+  desc "reload the database with seed data"
+  task :seed do
+    run "cd #{current_path}; rake db:seed RAILS_ENV=production"
+  end
+end
+
+after "deploy:update_code", :bundle_install
+desc "install the necessary prerequisites"
+task :bundle_install, :roles => :app do
+  run "cd #{release_path} && bundle install"
+end
