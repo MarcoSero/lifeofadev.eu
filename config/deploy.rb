1a4efad5b34493a0247f8f97fe26f58c5ffaf283
Marco Sero <marco@marcosero.com>
2012-09-07 22:55:21 +0200

    Capistrano >finally< works on my Amazon EC2 istance

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,17 @@
-# be sure to change these
+
 set :user, 'ubuntu'
-set :domain, 'lifeofadev.marcosero.com'
+set :domain, 'beta.lifeofadev.com'
 set :application, 'lifeofadev'
 
-# The location of the LOCAL repository relative to the current app
-set :repository,  "."
-# The way in which files will be transferred from repository to remote host
-# If you were using a hosted github repository this would be slightly different
-set :deploy_to, "/home/#{user}/public_html/#{application}"
+# the link of repository take advantage of file .ssh/config
+# to use the path of the private key
+# ms is just a shortcut for the host
+#   Host ms
+#   HostName marcosero.com
+#   User ubuntu
+#   IdentityFile ~/.ssh/marcosero.pem 
+set :repository, "ms:git/#{application}.git"
+set :deploy_to, "/home/#{user}/public_html/#{application}" 
 
 # distribute your applications across servers (the instructions below put them
 # all on the same server, defined above as 'domain', adjust as necessary)
@@ -16,8 +20,7 @@ role :web, domain
 role :db, domain, :primary => true
 
 # you might need to set this if you aren't seeing password prompts
-#default_run_options[:pty] = true
-
+# default_run_options[:pty] = true
 
 # As Capistrano executes in a non-interactive mode and therefore doesn't cause
 # any of your shell profile scripts to be run, the following might be needed
@@ -28,10 +31,6 @@ role :db, domain, :primary => true
 # default_environment['GEM_PATH']='<your paths>:/usr/lib/ruby/gems/1.8'
 
 # miscellaneous options
-ssh_options[:user] = "ubuntu"
-ssh_options[:keys] = ["#{ENV['HOME']}.ssh/marcosero.pem"] # make sure you also have the publickey
-ssh_options[:forward_agent] = true
-
 set :deploy_via, :remote_cache
 set :scm, 'git'
 set :branch, 'master'
