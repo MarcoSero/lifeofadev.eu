68823df4785c54613016a3cd0bf846dfe3627104
Marco Sero <marco@marcosero.com>
2012-09-23 18:25:26 +0200

    Updated deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 
 set :user, 'ubuntu'
-set :domain, 'lifeofadev.marcosero.com'
+set :domain, 'lifeofadev.eu'
 set :application, 'lifeofadev'
 
 # the link of repository take advantage of file .ssh/config
